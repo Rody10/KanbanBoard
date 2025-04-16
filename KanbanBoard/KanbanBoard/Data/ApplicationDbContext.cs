ff883c7e8783bca89cf85453e9ff97a24df3685f
Rody10 <rodneysimbatholanah@gmail.com>
2025-04-16 20:12:20 +0200

    added a KanbanBoardUser class that inherits from IndentityUser. I added FirstName and LastName fields, created a migration for this and updated the database

--- KanbanBoard/KanbanBoard/Data/ApplicationDbContext.cs
+++ KanbanBoard/KanbanBoard/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using KanbanBoard.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KanbanBoard.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<KanbanBoardUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- KanbanBoard/KanbanBoard/Program.cs
+++ KanbanBoard/KanbanBoard/Program.cs
@@ -1,4 +1,5 @@
 using KanbanBoard.Data;
+using KanbanBoard.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,7 @@ namespace KanbanBoard
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<KanbanBoardUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
