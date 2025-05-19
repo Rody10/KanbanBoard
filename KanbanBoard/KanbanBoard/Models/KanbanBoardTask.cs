e7c4853e3dc23043b0f94af886acd3d0e13a0ad2
Rody10 <rodneysimbatholanah@gmail.com>
2025-05-19 19:32:27 +0200

    Added a TaskStatus field to KnboardBoardTask

--- KanbanBoard/KanbanBoard/Models/KanbanBoardTask.cs
+++ KanbanBoard/KanbanBoard/Models/KanbanBoardTask.cs
@@ -5,9 +5,21 @@ namespace KanbanBoard.Models
 {
     public class KanbanBoardTask
     {
+        public enum Status
+        {
+            ToDo,
+            DoToday,
+            InProgress,
+            Done
+        }
+
         [Key]
         [Display(Name = "ID")]
         public int TaskId { get; set; }
+
+        [Display(Name = "Task Status")]
+        public Status TaskStatus { get; set; } = Status.ToDo;
+
         [Display(Name = "Name")]
         public string? TaskName { get; set; }
         [Display(Name = "Description")]
