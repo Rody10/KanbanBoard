c3abd69cd9bdfc5c0bc6a0d95642d367e11c5f18
Rody10 <rodneysimbatholanah@gmail.com>
2025-05-19 19:37:43 +0200

    Added migration for including TaskStatus field in KanbanBoardTask model

---
+++ KanbanBoard/KanbanBoard/Data/Migrations/20250519173556_AddedTaskStatusToKanbanBoardTaskModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KanbanBoard.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTaskStatusToKanbanBoardTaskModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TaskStatus",
+                table: "KanbanBoardTasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaskStatus",
+                table: "KanbanBoardTasks");
+        }
+    }
+}
